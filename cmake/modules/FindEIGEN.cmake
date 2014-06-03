61ad42d5ab67d9b7a0dc890ac7f22cf4d8cb3955
Andrey Shmakov <akshmakov@gmail.com>
2014-06-03 14:49:47 -0700

    fixed links against dolfin that caused MPI to break, poisson should build now

--- cmake/modules/FindEIGEN.cmake
+++ cmake/modules/FindEIGEN.cmake
@@ -2,10 +2,10 @@ message(STATUS "Checking for EIGEN3")
 
 
 if(NOT ${EIGEN_DIR})
-  set(EIGEN_DIR "/FIJEE-AUX")
+  set(EIGEN_DIR "/usr/include/eigen3")
 endif()
 
-set(EIGEN_INCLUDE_DIR "/FIJEE-AUX/include/eigen3/")
+set(EIGEN_INCLUDE_DIR "/usr/include/eigen3")
 
 
 set(EIGEN_FOUND 1)
