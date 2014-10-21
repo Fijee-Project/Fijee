a932e542d57f02f7850d67c60d8804c21f7a27e0
Yann Cobigo <yann.cobigo@yahoo.com>
2014-10-21 09:23:43 -0700

    Correction of CMake's and add fuzzy clustering in the minimization package.

--- Fijee/Utils/Minimizers/Kmeans_clustering.h
+++ Fijee/Utils/Minimizers/Kmeans_clustering.h
@@ -43,7 +43,7 @@ namespace Utils
   {
     namespace Clustering
     {
-     /*
+      /*
        * \brief Lloyd algorythm for k-means clustering
        *
        * This fonction compute the k-means clustering following Lloyd algorithm. 
@@ -53,103 +53,103 @@ namespace Utils
       template < typename T, typename U > void Lloyd_algorythm( const int N, const T* Points, int* Clusters,
 								const int n, double* Mu,
 								double Epsilon = 1.e-01 )
-	{
-	  // 
-	  // Time log
-	  FIJEE_TIME_PROFILER("Utils::Minimizers::Kmeans_clustering::Lloyd_algorythm");
+      {
+	// 
+	// Time log
+	FIJEE_TIME_PROFILER("Utils::Minimizers::Kmeans_clustering::Lloyd_algorythm");
 
-	  // 
-	  // Convergence criteria
-	  double L_kmeans_old = 1.e+06;
-	  double L_kmeans     = 0.;
-	  int    iteration    = 0;
+	// 
+	// Convergence criteria
+	double L_kmeans_old = 1.e+06;
+	double L_kmeans     = 0.;
+	int    iteration    = 0;
 	  
-	  // 
-	  // Determin Max and Min from Points
-	  U 
-	    max = 0,
-	    min = 255;
-	  // 
-	  for ( int i = 0 ; i < N ; i++ )
-	    if( Points[i] > max ) max = static_cast<U>( Points[i] );
-	    else if( Points[i] < min ) min = static_cast<U>( Points[i] );
+	// 
+	// Determin Max and Min from Points
+	U 
+	  max = 0,
+	  min = 255;
+	// 
+	for ( int i = 0 ; i < N ; i++ )
+	  if( Points[i] > max ) max = static_cast<U>( Points[i] );
+	  else if( Points[i] < min ) min = static_cast<U>( Points[i] );
 	
-//	  // 
-//	  std::uniform_int_distribution<U> distribution( min, max );
-	  // 
-	  double set_size = max - min;
-	  double domaine  = set_size / (double) n;
-	  double min_max_cluster[n][2];
-	  // positions Mu in the center of domaines
-	  for( int j = 0 ; j < n ; j++ )
-	    {
-	      Mu[j] = domaine/2. + j*domaine;
-	      std::cout << Mu[j] << " ";}
-	  std::cout << std::endl;
+	//	  // 
+	//	  std::uniform_int_distribution<U> distribution( min, max );
+	// 
+	double set_size = max - min;
+	double domaine  = set_size / (double) n;
+	double min_max_cluster[n][2];
+	// positions Mu in the center of domaines
+	for( int j = 0 ; j < n ; j++ )
+	  {
+	    Mu[j] = domaine/2. + j*domaine;
+	    std::cout << Mu[j] << " ";}
+	std::cout << std::endl;
 
-	  // 
-	  // Minimization loop
-	  while( fabs(L_kmeans - L_kmeans_old) > Epsilon )
-	    {
-	      // 
-	      // 
-	      iteration++;
-	      // 
-	      L_kmeans_old = L_kmeans;
-	      L_kmeans = 0;
+	// 
+	// Minimization loop
+	while( fabs(L_kmeans - L_kmeans_old) > Epsilon )
+	  {
+	    // 
+	    // 
+	    iteration++;
+	    // 
+	    L_kmeans_old = L_kmeans;
+	    L_kmeans = 0;
 
-	      // 
-	      // Determins r_ik (Clusters)
-	      for( int i = 0 ; i < N ; i++ )
-		{
-		  double min_dist = 1.e+09;
-		  // 
-		  for ( int j = 0 ; j < n ; j++)
-		    if( fabs(Points[i] - Mu[j]) < min_dist )
-		      {
-			Clusters[i] = j;
-			min_dist = fabs( Points[i] - Mu[j] );
-		      }
-		}
+	    // 
+	    // Determins r_ik (Clusters)
+	    for( int i = 0 ; i < N ; i++ )
+	      {
+		double min_dist = 1.e+09;
+		// 
+		for ( int j = 0 ; j < n ; j++)
+		  if( fabs(Points[i] - Mu[j]) < min_dist )
+		    {
+		      Clusters[i] = j;
+		      min_dist = fabs( Points[i] - Mu[j] );
+		    }
+	      }
 
-	      // 
-	      // Recompute Mu
-	      double 
-		Num = 0., 
-		Den = 0.;
-	      for( int j = 0 ; j < n ; j++ )
-		{
-		  Num = 0;
-		  Den = 0;
-		  //	    
-		  for( int i = 0 ; i < N ; i++ )
-		    if( Clusters[i] == j )
-		      {
-			Num += Points[i];
-			Den++;
-		      }
-		  //
-		  Mu[j]  = static_cast<double>( Num );
-		  Mu[j] /= static_cast<double>( Den );
-		}
-
-	      // 
-	      // Compute the Lagrangien
-	      for( int j = 0 ; j < n ; j++ )
+	    // 
+	    // Recompute Mu
+	    double 
+	      Num = 0., 
+	      Den = 0.;
+	    for( int j = 0 ; j < n ; j++ )
+	      {
+		Num = 0;
+		Den = 0;
+		//	    
 		for( int i = 0 ; i < N ; i++ )
 		  if( Clusters[i] == j )
-		    L_kmeans += (Points[i]-Mu[j])*(Points[i]-Mu[j]);
-	    }
+		    {
+		      Num += Points[i];
+		      Den++;
+		    }
+		//
+		Mu[j]  = static_cast<double>( Num );
+		Mu[j] /= static_cast<double>( Den );
+	      }
+
+	    // 
+	    // Compute the Lagrangien
+	    for( int j = 0 ; j < n ; j++ )
+	      for( int i = 0 ; i < N ; i++ )
+		if( Clusters[i] == j )
+		  L_kmeans += (Points[i]-Mu[j])*(Points[i]-Mu[j]);
+	  }
 
 
-	  //
-	  //
-	  std::cout << std::endl;
-	  std::cout << iteration << std::endl;
-	  for( int j = 0 ; j < n ; j++ ){
-	    std::cout << Mu[j] << std::endl;}
-	  std::cout << std::endl;
-	}
+	//
+	//
+	std::cout << std::endl;
+	std::cout << iteration << std::endl;
+	for( int j = 0 ; j < n ; j++ ){
+	  std::cout << Mu[j] << std::endl;}
+	std::cout << std::endl;
+      }
       /*
        * \brief Fuzzy c-means clustering
        *
@@ -162,189 +162,223 @@ namespace Utils
 							      const int n/*num od clust*/, double* Mu,
 							      const Utils::Data_structure::Graph_abstract_data_type<U> Neighborhood_system, bool Use_ns = false, 
 							      double Epsilon = 1.e-01 )
-	{
-	  // 
-	  // Time log
-	  FIJEE_TIME_PROFILER("Utils::Minimizers::Kmeans_clustering::Fuzzy_c_means");
+      {
+	// 
+	// Time log
+	FIJEE_TIME_PROFILER("Utils::Minimizers::Kmeans_clustering::Fuzzy_c_means");
 
-	  // 
-	  // Convergence criteria
-	  double L_cmeans_old = 1.e+06;
-	  double L_cmeans     = 0.;
-	  int    iteration    = 0;
+	// 
+	// Convergence criteria
+	double L_cmeans_old = 1.e+06;
+	double L_cmeans     = 0.;
+	int    iteration    = 0;
 	  
-	  // 
-	  // Membership matrix
-	  double* U_ji[n];
-	  double* H_ji[n];
+	// 
+	// Membership matrix
+	double* U_ji[n];
+	double* H_ji[n];
+	//
+	double *x = new double[N];
 
-	  // 
-	  // Determin Max and Min from Points
-	  U 
-	    max = 0,
-	    min = 255;
-	  // 
-	  for ( int i = 0 ; i < N ; i++ )
-	    if( Points[i] > max ) max = static_cast<U>( Points[i] );
-	    else if( Points[i] < min ) min = static_cast<U>( Points[i] );
+
+	// 
+	// Determin Max and Min from Points
+	U 
+	  max = 0,
+	  min = 255;
+	// 
+	for ( int i = 0 ; i < N ; i++ )
+	  if( Points[i] > max ) max = static_cast<U>( Points[i] );
+	  else if( Points[i] < min ) min = static_cast<U>( Points[i] );
 	
-//	  // 
-	  // 
-	  double set_size = max - min;
-	  double domaine  = set_size / (double) n;
-	  double min_max_cluster[n][2];
-	  // positions Mu in the center of domaines
-	  for( int j = 0 ; j < n ; j++ )
-	    {
-	      U_ji[j] = new double[N];
-	      H_ji[j] = new double[N];
-	      // 
-	      Mu[j] = domaine/2. + j*domaine;
-	      // 
-	      std::cout << Mu[j] << " ";
-	    }
-	  std::cout << std::endl;
+	// 
+	// 
+	double set_size = max - min;
+	double domaine  = set_size / (double) n;
+	double min_max_cluster[n][2];
+	// positions Mu in the center of domaines
+	for( int j = 0 ; j < n ; j++ )
+	  {
+	    U_ji[j] = new double[N];
+	    H_ji[j] = new double[N];
+	    // 
+	    Mu[j] = domaine/2. + j*domaine;
+	    // 
+	    std::cout << Mu[j] << " ";
+	  }
+	std::cout << std::endl;
 
 
-	  // 
-	  // Minimization loop
-	  while( 100 * fabs(L_cmeans-L_cmeans_old) / L_cmeans > Epsilon )
-	    {
-	      // 
-	      // 
-	      iteration++;
-	      std::cout 
-		<< "it: " << iteration << " Cost function L - " 
-		<< "L_old: " << L_cmeans_old << " " 
-		<< "L_new: " << L_cmeans << " " 
-		<< 100 * fabs(L_cmeans-L_cmeans_old) / L_cmeans << "%"
-		<< std::endl;
+	// 
+	// Minimization loop
+	while( 100 * fabs(L_cmeans-L_cmeans_old) / L_cmeans > Epsilon )
+	  {
+	    // 
+	    // 
+	    iteration++;
+	    std::cout 
+	      << "it: " << iteration << " Cost function L - " 
+	      << "L_old: " << L_cmeans_old << " " 
+	      << "L_new: " << L_cmeans << " " 
+	      << 100 * fabs(L_cmeans-L_cmeans_old) / L_cmeans << "%"
+	      << std::endl;
 
-	      // 
-	      L_cmeans_old = L_cmeans;
-	      L_cmeans = 0;
+	    // 
+	    L_cmeans_old = L_cmeans;
+	    L_cmeans = 0;
 
-	      // 
-	      // Determins U_ji (Clusters)
-	      for( int i = 0 ; i < N ; i++ )
+	    // 
+	    // Determins U_ji (Clusters)
+	    for( int i = 0 ; i < N ; i++ )
+	      {
+		double 
+		  x_i         = Points[i],
+		  Denominator = 0.;
+		// 
+		for( int k = 0 ; k < n ; k++ )
+		  {
+		    double deno  = fabs(x_i - Mu[k]);
+		    Denominator += 1./(deno*deno);
+		  }
+		// 
 		for( int j = 0 ; j < n ; j++ )
 		  {
-		    double 
-		      x_i         = Points[i],
-		      numerator   = fabs(x_i - Mu[j]),
-		      Denominator = 0.;
+		    double numerator = fabs(x_i - Mu[j]);
 		    // 
-		    for( int k = 0 ; k < n ; k++ )
-		      Denominator += fabs(x_i - Mu[k]) * fabs(x_i - Mu[k]);
-		    // 
-		    U_ji[j][i] = numerator*numerator / Denominator;
-//		    std::cout << " U_ji["<<j<<"]["<<i<<"]=" << U_ji[j][i] << std::endl;
+		    numerator *= numerator*Denominator;
+		    U_ji[j][i] = 1. / numerator;
 		  }
+	      }
 
-	      if(Use_ns)
-		{
-		  // 
-		  // Determins H_ji
-		  for( int w = 0 ; w < 256 ; w++ )
-		    for( int v = 0 ; v < 256 ; v++ )
-		      for( int u = 0 ; u < 256 ; u++ )
-			{
-			  int i = u + 256*v + 256*256*w;
-			  // 
-			  for( int j = 0 ; j < n ; j++ )
-			    {
-			      H_ji[j][i] = 0.;
-			      for( auto vertex : Neighborhood_system.get_vertices_() )
-				if( u + vertex.x() >= 0 && u + vertex.x() < 256 &&
-				    v + vertex.y() >= 0 && v + vertex.y() < 256 &&
-				    w + vertex.z() >= 0 && w + vertex.z() < 256 )
-				  {
-				    int k = u+vertex.x() + 256*(v+vertex.y()) + 256*256*(w+vertex.z());
-				    H_ji[j][i] += U_ji[j][k];
-//				    std::cout << "H_ji["<<j<<"]["<<i<<"]=" <<  H_ji[j][i] << " U_ji["<<j<<"]["<<k<<"]=" << U_ji[j][k] << std::endl;
-				  }
-			    }
-			}
-		}
-
-
-	      // 
-	      // Recompute Mu
-	      double 
-		Num = 0., 
-		Den = 0.;
-	      for( int j = 0 ; j < n ; j++ )
-		{
-		  Num = 0;
-		  Den = 0;
-		  //	    
-		  for( int i = 0 ; i < N ; i++ )
-		    {
-		      if(Use_ns)
-			{
-			  //			  std::cout << "av U_ji["<<j<<"]["<<i<<"]: " << U_ji[j][i] << " H_ji[j][i]=" << H_ji[j][i] << std::endl;
-			  // 
-			  // Update U_ji with H_ji
-//			  U_ji[j][i] = U_ji[j][i]*H_ji[j][i];
-			  U_ji[j][i] *= H_ji[j][i];
-			  double new_denominator = 0.;
-			  // 
-			  for( int jj = 0 ; jj < n ; jj++ )
-			    {
-			      new_denominator = /*U_ji[jj][i]**/H_ji[jj][i];
-//     std::cout << H_ji[jj][i] << " "  << U_ji[jj][i] << " " << new_U << " " << std::endl;
-			    }
-			  
-			  // 
-			  U_ji[j][i] /= new_denominator;
-			  //			  std::cout << "ap U_ji["<<j<<"]["<<i<<"]: " << U_ji[j][i] << std::endl;
-			}
-
-		      // 
-		      // 
-		      Num += U_ji[j][i]*U_ji[j][i]*static_cast<double>(Points[i]);
-		      Den += U_ji[j][i]*U_ji[j][i];
-		    }
-		  //
-		  Mu[j]  = Num / Den;
-		}
-
-	      // 
-	      // Compute the Lagrangien
-	      for( int i = 0 ; i < N ; i++ )
-		{
-		  double cluster_membership = 0.;
-		  for( int j = 0 ; j < n ; j++ )
-		    {
-		      L_cmeans += U_ji[j][i]*U_ji[j][i]*(Points[i]-Mu[j])*(Points[i]-Mu[j]);
-		      //
-		      if( cluster_membership < U_ji[j][i] ) 
-			{
-			  cluster_membership = U_ji[j][i];
-			  Clusters[i] = j;
-			}
-		    }
-		}
-	    }
+	    if(Use_ns&&iteration>20)
+	      {
+		// 
+		// Determins H_ji
+		for( int w = 0 ; w < 256 ; w++ )
+		  for( int v = 0 ; v < 256 ; v++ )
+		    for( int u = 0 ; u < 256 ; u++ )
+		      {
+			int i      = u + 256*v + 256*256*w;
+			double prob_max = 0.;
+			// 
+			for( int j = 0 ; j < n ; j++ )
+			  {
+			    H_ji[j][i] = 1.;
+			    for( auto vertex : Neighborhood_system.get_vertices_() )
+			      if( u + vertex.x() >= 0 && u + vertex.x() < 256 &&
+				  v + vertex.y() >= 0 && v + vertex.y() < 256 &&
+				  w + vertex.z() >= 0 && w + vertex.z() < 256 )
+				{
+				  int k = u+vertex.x() + 256*(v+vertex.y()) + 256*256*(w+vertex.z());
+				  H_ji[j][i] *= 256. - fabs(Mu[Clusters[i]]-Mu[Clusters[k]]);
+				  H_ji[j][i] /= 256.;
+				  //				    H_ji[j][i] += U_ji[j][k];
+//				  std::cout << "H_ji["<<j<<"]["<<i<<"]=" <<  H_ji[j][i] 
+//					    << " Mu["<<Clusters[i]<<"]= " << Mu[Clusters[i]]
+//					    << " Mu["<<Clusters[k]<<"]= " << Mu[Clusters[k]]
+//					    << std::endl;
+				}
+			    // 
+			    if( H_ji[j][i] > prob_max )
+			      prob_max = H_ji[j][i];
+			  }
+			// 
+			x[i] = 1./prob_max;
+		      }
+	      }
 
 
-	  //
-	  //
-	  std::cout << std::endl;
-	  std::cout << iteration << std::endl;
-	  for( int j = 0 ; j < n ; j++ ){
-	    std::cout << Mu[j] << std::endl;}
-	  std::cout << std::endl;
+	    // 
+	    // Recompute Mu
+	    double 
+	      Num = 0., 
+	      Den = 0.;
+	    for( int j = 0 ; j < n ; j++ )
+	      {
+		Num = 0;
+		Den = 0;
+		//	    
+		for( int i = 0 ; i < N ; i++ )
+		  {
+		    if(Use_ns&&iteration>20)
+		      {
+//			if( x[i] != 1 )
+//			  std::cout 
+//			    << "av U_ji["<<j<<"]["<<i<<"]: " << U_ji[j][i] 
+//			    << " H_ji[j][i]=" << H_ji[j][i] 
+//			    << " x[i]=" << x[i] << std::endl;
+			// 
+			// Update U_ji with H_ji
+			//			  U_ji[j][i] = U_ji[j][i]*H_ji[j][i];
+			U_ji[j][i] *= x[i]*H_ji[j][i];
+//			if( x[i] != 1 )
+//			  std::cout 
+//			    << "ap U_ji["<<j<<"]["<<i<<"]: " << U_ji[j][i] 
+//			    << " H_ji[j][i]=" << H_ji[j][i]
+//			    << " x[i]=" << x[i] << std::endl;
+			double new_denominator = 0.;
+			//			  // 
+			//			  for( int jj = 0 ; jj < n ; jj++ )
+			//			    {
+			//			      new_denominator = /*U_ji[jj][i]**/H_ji[jj][i];
+			////     std::cout << H_ji[jj][i] << " "  << U_ji[jj][i] << " " << new_U << " " << std::endl;
+			//			    }
+			//			  
+			//			  // 
+			//			  U_ji[j][i] /= new_denominator;
+			//			  //			  std::cout << "ap U_ji["<<j<<"]["<<i<<"]: " << U_ji[j][i] << std::endl;
+		      }
+
+		    // 
+		    // 
+		    Num += U_ji[j][i]*U_ji[j][i]*static_cast<double>(Points[i]);
+		    Den += U_ji[j][i]*U_ji[j][i];
+		  }
+		//
+		Mu[j]  = Num / Den;
+	      }
+
+	    // 
+	    // Compute the Lagrangien
+	    for( int i = 0 ; i < N ; i++ )
+	      {
+		double cluster_membership = 0.;
+		for( int j = 0 ; j < n ; j++ )
+		  {
+		    L_cmeans += U_ji[j][i]*U_ji[j][i]*(Points[i]-Mu[j])*(Points[i]-Mu[j]);
+		    //
+		    if( cluster_membership < U_ji[j][i] ) 
+		      {
+			cluster_membership = U_ji[j][i];
+			Clusters[i] = j;
+		      }
+		  }
+	      }
+	  }
 
 
-	  // 
-	  // 
-	  for( int j = 0 ; j < n ; j++ )
-	    {
-	      delete[] U_ji[j];
-	      U_ji[j] = nullptr;
-	    }
-	}
+	//
+	//
+	std::cout << std::endl;
+	std::cout << iteration << std::endl;
+	for( int j = 0 ; j < n ; j++ ){
+	  std::cout << Mu[j] << std::endl;}
+	std::cout << std::endl;
+
+
+	// 
+	// 
+	for( int j = 0 ; j < n ; j++ )
+	  {
+	    delete[] U_ji[j];
+	    U_ji[j] = nullptr;
+	    delete[] H_ji[j];
+	    H_ji[j] = nullptr;
+	  }
+	// 
+	delete[] x;
+	x = nullptr;
+      }
     }
   }
 }
--- cmake/modules/FindNIFTI.cmake
+++ cmake/modules/FindNIFTI.cmake
@@ -17,42 +17,54 @@
 
 
 # INCLUDE directory
-FIND_PATH(NIFTI_INCLUDE_DIR
-    NAMES nifti1.h
-    PATH_SUFFIXES nifti
-    DOC "The include directory containing nifti1.h"
-    )
+find_path( NIFTI_INCLUDE_DIR nifti1.h
+  ${NIFTI_DIR}/include/nifti
+  /opt/local/include
+  /usr/local/include
+  /usr/include
+  /usr/include/nifti
+  )
+if( NIFTI_INCLUDE_DIR )
+  set( NIFTI_FOUND "YES" )
+endif( NIFTI_INCLUDE_DIR )
 
-# LIBRARY files
-FIND_LIBRARY(NIFTI_NIFTICDF_LIBRARY
-    NAMES nifticdf
-    DOC "The library file libnifticdf.so"
-    )
+mark_as_advanced( NIFTI_INCLUDE_DIR )
 
-FIND_LIBRARY(NIFTI_NIFTIIO_LIBRARY
-    NAMES niftiio
-    DOC "The library file libniftiiof.so"
-    )
-    
-FIND_LIBRARY(NIFTI_ZNZ_LIBRARY
-    NAMES znz
-    DOC "The library file libznz.so"
-    )
-
-# handle the QUIETLY and REQUIRED arguments and set PNG_FOUND to TRUE if 
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(NIFTI
-    "Cannot find package NIFTI. Did you install the package libnifti1-dev?"
-    NIFTI_INCLUDE_DIR
-    NIFTI_NIFTICDF_LIBRARY
-    NIFTI_NIFTIIO_LIBRARY
-    NIFTI_ZNZ_LIBRARY
-    )
-
-# these variables are only visible in 'advanced mode' 
-MARK_AS_ADVANCED(NIFTI_INCLUDE_DIR
-    NIFTI_NIFTICDF_LIBRARY
-    NIFTI_NIFTIIO_LIBRARY
-    NIFTI_ZNZ_LIBRARY
-    )
+#Find_path( NIFTI_INCLUDE_DIR nifti1.h
+#  PATH_SUFFIXES nifti
+#  DOC "The include directory containing nifti1.h"
+#  )
+#
+## LIBRARY files
+#FIND_LIBRARY(NIFTI_NIFTICDF_LIBRARY
+#    NAMES nifticdf
+#    DOC "The library file libnifticdf.so"
+#    )
+#
+#FIND_LIBRARY(NIFTI_NIFTIIO_LIBRARY
+#    NAMES niftiio
+#    DOC "The library file libniftiiof.so"
+#    )
+#    
+#FIND_LIBRARY(NIFTI_ZNZ_LIBRARY
+#    NAMES znz
+#    DOC "The library file libznz.so"
+#    )
+#
+## handle the QUIETLY and REQUIRED arguments and set PNG_FOUND to TRUE if 
+## all listed variables are TRUE
+#INCLUDE(FindPackageHandleStandardArgs)
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(NIFTI
+#    "Cannot find package NIFTI. Did you install the package libnifti1-dev?"
+#    NIFTI_INCLUDE_DIR
+#    NIFTI_NIFTICDF_LIBRARY
+#    NIFTI_NIFTIIO_LIBRARY
+#    NIFTI_ZNZ_LIBRARY
+#    )
+#
+## these variables are only visible in 'advanced mode' 
+#MARK_AS_ADVANCED(NIFTI_INCLUDE_DIR
+#    NIFTI_NIFTICDF_LIBRARY
+#    NIFTI_NIFTIIO_LIBRARY
+#    NIFTI_ZNZ_LIBRARY
+#    )
